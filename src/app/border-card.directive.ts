54d947440b711929e9d341ba7ed484420248d042
LawsonGod <godwill.lawson@gmail.com>
2024-05-13 15:29:02 +0200

    Ajout d'une directive d'attribut pour les border des cards

---
+++ src/app/border-card.directive.ts
@@ -0,0 +1,40 @@
+import {Directive, ElementRef, HostListener, Input} from '@angular/core';
+
+
+@Directive({
+  selector: '[pkmnBorderCard]',
+  standalone: true,
+
+})
+export class BorderCardDirective {
+  private initialColor: string = '#f5f5f5';
+  private defaultColor: string = '#009688';
+  private defaultHeight: number = 200;
+
+  constructor(private el: ElementRef) { 
+    this.setHeight(this.defaultHeight);
+    this.setBorder(this.defaultColor);
+    console.log('Directive pkmnBorderCard instantiated');
+  }
+  @Input ('pkmnBorderCard') borderColor: string;
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.setBorder(this.borderColor || this.defaultColor);
+    console.log('Mouse enter');
+
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.setBorder(this.initialColor);
+    console.log('Mouse leave');
+
+  }
+    
+  private setHeight(height: number){
+    this.el.nativeElement.style.height = height + 'px';
+  }
+
+  private setBorder(color: string){
+    this.el.nativeElement.style.border = 'solid 4px ' + color;
+  }
+
+}
